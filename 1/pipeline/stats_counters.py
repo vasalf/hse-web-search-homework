e560b935f808edf09e9a74f35b9bb0becaeb3f01
Ann <annikura@yandex.ru>
2019-09-10 23:56:55 +0300

    Add tf-idf metrics.

--- 1/pipeline/stats_counters.py
+++ 1/pipeline/stats_counters.py
@@ -1,4 +1,7 @@
+import math
+
 import nltk
+import operator
 
 nltk.download("stopwords")
 
@@ -57,8 +60,37 @@ class SimpleStats(PipelineStage):
 
     def dump(self):
         print("English words rate: {}, {} out of {}"
-              .format(self.english_words / (self.english_words + self.not_english_words), self.english_words, (self.english_words + self.not_english_words)))
+              .format(self.english_words / (self.english_words + self.not_english_words), self.english_words,
+                      (self.english_words + self.not_english_words)))
         print("Average word length: all words: {}, (belo)russian: {}, english: {}."
-              .format((self.english_lengths_sum + self.not_english_lengths_sum) / (self.english_words + self.not_english_words),
-                      self.not_english_lengths_sum / self.not_english_words,
-                      self.english_lengths_sum / self.english_words))
+              .format(
+            (self.english_lengths_sum + self.not_english_lengths_sum) / (self.english_words + self.not_english_words),
+            self.not_english_lengths_sum / self.not_english_words,
+            self.english_lengths_sum / self.english_words))
+
+
+class DictionaryStats(PipelineStage):
+    __TOP_FREQUENCIES = 10000
+    __TOP_IDFS = 10000
+
+    def __init__(self):
+        self.tf = {}
+        self.documents_count = 0
+        self.idf = {}
+
+    def accept(self, consumer_input: PipedInput):
+        for word in set(consumer_input.get_text()):
+            self.idf[word] = self.idf.get(word, 0) + 1
+        for word in consumer_input.get_text():
+            self.tf[word] = self.tf.get(word, 0) + 1
+        self.documents_count += 1
+
+    def dump(self):
+        sorted_frequencies = sorted(self.tf.items(), key=operator.itemgetter(1), reverse=True)
+        idfs = sorted([(k, math.log(self.documents_count / v)) for k, v in self.idf.items()],
+                      key=operator.itemgetter(1), reverse=True)[:self.__TOP_IDFS]
+
+        with open("top_frequent_words.txt", 'w') as file:
+            file.writelines(map(lambda x: "{} {}\n".format(x[0], x[1]), sorted_frequencies[:self.__TOP_FREQUENCIES]))
+        with open("top_idf_words.txt", 'w') as file:
+            file.writelines(map(lambda x: "{} {}\n".format(x[0], x[1]), idfs[:self.__TOP_IDFS]))
