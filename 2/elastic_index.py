0857fe58a590ada640499338c21fabca013fa213
Vasily Alferov <ya-ikmik2012@yandex.ru>
2019-09-23 20:36:03 +0300

    Start new lab

---
+++ 2/elastic_index.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python3
+
+import elasticsearch
+import elasticsearch.helpers as esh
+import errno
+import json
+import re
+import os
+import os.path
+import timeit
+import xml.dom.minidom as minidom
+
+DOC_DIR = "extracted"
+DOC_RE = re.compile("(\d+).txt")
+INDEX_BODY = {
+    "mappings": {
+        "properties": {
+            "title": { "type": "text" },
+            "body": { "type": "text" }
+        }
+    }
+}
+
+class Document:
+    def indexAction(self):
+        return {
+            "_op_type": "index",
+            "_index": self.index,
+            "_type": "document",
+            "_id": self.id,
+            "_source": {
+                "url": self.url
+            },
+            "title": self.title,
+            "body": self.content
+        }
+
+
+class ParsedDocument(Document):
+    def __init__(self, fn, doc, meta):
+        with open(doc, "r") as docf:
+            self.content = docf.read()
+        with open(meta, "r") as metaf:
+            metaj = json.load(metaf)
+        self.title = metaj["title"]
+        self.url = metaj["url"]
+        self.id = int(fn)
+        self.index = "extracted"
+
+
+def genSelfExtractedActions():
+    for fn in os.listdir(DOC_DIR):
+        match = DOC_RE.match(fn)
+        if match:
+            doc_fn = os.path.join(DOC_DIR, fn)
+            meta_fn = os.path.join(DOC_DIR, "{}.json".format(match.group(1)))
+            yield ParsedDocument(match.group(1), doc_fn, meta_fn).indexAction()
+
+
+
+class MeasuredAction:
+    def __init__(self, pfx):
+        self.pfx = pfx
+
+    def __call__(self, action):
+        def wrapped(*args, **kwargs):
+            start = timeit.default_timer()
+            ret = action(*args, **kwargs)
+            end = timeit.default_timer()
+            print("{} took {} s".format(self.pfx, end - start))
+            return ret
+        return wrapped
+
+
+@MeasuredAction("Indexing of extracted documents")
+def indexExtracted(es):
+    esh.bulk(es, genSelfExtractedActions())
+
+
+def indexSize(es, index):
+    return es.indices.stats(index=index)["_all"]["primaries"]["store"]["size_in_bytes"]
+
+
+def main():
+    es = elasticsearch.Elasticsearch()
+
+    es.indices.create(index="extracted", body=INDEX_BODY, ignore=400)
+
+    indexExtracted(es)
+    print("Size of index of extracted documents: {} bytes".format(indexSize(es, "extracted")))
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Информационный поиск, осень 2019
 ## Презентация 1
 https://docs.google.com/presentation/d/1bf1NIfIbrrcaTGSlOaybHXRr4nkV-lhPgWj_hTOPLro/edit?usp=sharing
+
+### ДЗ 2
+**Это нужно будет удалить перед сдачей ДЗ!**
+
+Поставьте себе ластик, икебану, и вот это всё.
+[Дока](https://www.elastic.co/guide/en/elasticsearch/reference/current/getting-started-install.html) по установке.
