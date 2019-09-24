e7cad0d3d57757d1d53c494fed1ff7364363d763
dkaznacheev <dmitry.kaznacheev@gmail.com>
2019-09-24 20:43:18 +0300

    Added r-precision

--- 2/rate.py
+++ 2/rate.py
@@ -23,14 +23,36 @@ def try_divide(a, b):
         return "n/a"
     return a / b
 
+def map_at_k(expected, actual_raw, k):
+    if len(actual_raw) == 0:
+        return "n/a"
+    
+    p = 0
+    for i in range(1, k):
+        p_i, _ = precision_recall(expected, actual_raw[:i])
+        p += p_i
+    return p / k
+
+def precision_recall(expected, actual_raw):
+    actual = set([doc["id"] for doc in actual_raw])
+    true_pos = len(actual.intersection(expected))
+    
+    p = try_divide(true_pos, len(actual))
+    r = try_divide(true_pos, len(expected))
+    return p, r
+    
+
 def main():
-    if len(sys.argv) < 2:
-        result_path = "results/basic.json"
+    if len(sys.argv) < 3:
+        basic_path = "results/basic.json"
+        rprecision_path = "results/rprecision.json"
     else:
-        result_path = sys.argv[1]
+        basic_path = sys.argv[1]
+        rprecision_path = sys.argv[2]
     
     expected_raw = minidom.parse(RELEVANT_TABLE).getElementsByTagName("task")
-    actual_raw = load_json(result_path)
+    actual_raw = load_json(basic_path)
+    actual_rprecision_raw = load_json(rprecision_path)
     
     metrics = {}
     for task in expected_raw:
@@ -39,15 +61,17 @@ def main():
             continue
             
         expected = set([doc.getAttribute("id") 
-                for doc in task.getElementsByTagName("document") 
-                if doc.getAttribute("relevance") == "vital"])
-        actual = set([doc["id"] for doc in actual_raw[qid]])
+                        for doc in task.getElementsByTagName("document") 
+                        if doc.getAttribute("relevance") == "vital"])
+                    
+        p20, r20 = precision_recall(expected, actual_raw[qid])    
+        map20 = map_at_k(expected, actual_raw[qid], 20)
+        rp, _ = precision_recall(expected, actual_rprecision_raw[qid])
         
-        true_pos = len(actual.intersection(expected))
-        metrics[qid] = { "p20": try_divide(true_pos, len(actual)), "r20": try_divide(true_pos, len(expected)) }
-        print("{}: p@20={} r@20={}".format(qid, metrics[qid]['p20'], metrics[qid]['r20']))
+        metrics[qid] = { "p20": p20, "r20": r20, "map20": map20, "rprecision": rp }
+        print("{}: p@20={} r@20={} map@20={}, rprec={}".format(qid, p20, r20, map20, rp))
         
-    with open(os.path.join("results", "{}.json".format("pr20")), 'w') as file:
+    with open(os.path.join("results", "metrics.json"), 'w') as file:
         json.dump(metrics, file)
         
 if __name__ == "__main__":
--- 2/search.py
+++ 2/search.py
@@ -14,38 +14,54 @@ import sys
 QUERIES_XML = "web2008_adhoc.xml"
 RELEVANT_TABLE = "or_relevant-minus_table.xml"
 
-def es_search_basic(es, q):
-    return es.search(size=20, index="extracted", body={"_source":["_score", "_id"], "query": {"simple_query_string": {"query": q, "fields": ["body"]}}})
+def es_search_basic(es, query):
+    return es.search(size=20, index="extracted", body={"_source":["_score", "_id"], "query": {"match": {"body": query["qtext"]}}})
 
+def es_search_rprecision(es, query):
+    return es.search(size=query["size"], index="extracted", body={"_source":["_score", "_id"], "query": {"match": {"body": query["qtext"]}}})
+    
 def get_query_info(query):
     qid = query.getAttribute("id")
     qtext = query.getElementsByTagName("querytext")[0].firstChild.data
-    return qid, qtext.replace("\n", "")
+    return qid, qtext
 
 def run_search(es, queries, search, outname):
     results = {}
-    for i, (qid, qtext) in enumerate(queries):
+    for i, query in enumerate(queries):
         try:
-            print("running query {}/{} ({}): {}".format(i, len(queries), qid, qtext))
-            search_result = search(es, qtext)
-            results[qid] = [ {"score": hit['_score'], "id": hit["_id"]} for hit in search_result["hits"]["hits"] ]
+            print("{}: query {}/{} ({}): {}".format(outname, i, len(queries), query["qid"], query["qtext"]))
+            search_result = search(es, query)
+            results[query["qid"]] = [ {"score": hit['_score'], "id": hit["_id"]} for hit in search_result["hits"]["hits"] ]
         except Exception as e:
-            print("query {} failed: {}".format(qid, qtext), file=sys.stderr)
+            print("query failed: {}".format(query), file=sys.stderr)
             print(e, file=sys.stderr)
     with open(os.path.join("results", "{}.json".format(outname)), 'w') as file:
         json.dump(results, file)
 
+def get_relevant_sizes(tasks):
+    relevant_sizes = {}
+    for task in tasks:
+        qid = task.getAttribute("id")
+        relevant_size = len([doc.getAttribute("id") 
+                            for doc in task.getElementsByTagName("document") 
+                            if doc.getAttribute("relevance") == "vital"])
+        relevant_sizes[qid] = relevant_size
+    return relevant_sizes
+        
+
 def get_queries():
-    task_ids = set([task.getAttribute("id") for task in minidom.parse(RELEVANT_TABLE).getElementsByTagName("task")])
+    tasks = minidom.parse(RELEVANT_TABLE).getElementsByTagName("task")
+    relevant_sizes = get_relevant_sizes(tasks)
+    
     all_queries = minidom.parse(QUERIES_XML).getElementsByTagName("task")
     queries = []
     for i, query in enumerate(all_queries):
         qid, qtext = get_query_info(query)
-        if qid in task_ids:
+        if qid in relevant_sizes:
             print("{} queries loaded".format(i + 1), end="\r")
-            queries.append((qid, qtext))
+            queries.append({ "qid": qid, "qtext": qtext, "size": relevant_sizes[qid] })
     return queries
-        
+
 def main():
     host = os.getenv("ELASTIC_HOST")
     passwd = os.getenv("ELASTIC_PASS")
@@ -57,6 +73,6 @@ def main():
     queries = get_queries()
     print("{} queries loaded".format(len(queries)))
     run_search(es, queries, es_search_basic, "basic")
-    
+    run_search(es, queries, es_search_rprecision, "rprecision")
 if __name__ == "__main__":
     main()
